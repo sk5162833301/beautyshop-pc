933f54b557c619aa355c6636afa40e0e552dc349
sukun.ex <sukun.ex@hx-partners.com>
2021-09-14 22:41:44 +0800

    layout布局

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,111 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <a-layout id="components-layout-demo-responsive">
+    <a-layout-sider
+      breakpoint="lg"
+      collapsed-width="0"
+      @collapse="onCollapse"
+      @breakpoint="onBreakpoint"
+    >
+      <div class="logo" />
+      <a-menu mode="inline" :open-keys="openKeys" @openChange="onOpenChange">
+        <a-sub-menu key="sub1">
+          <span slot="title"><a-icon type="mail" /><span>Navigation One</span></span>
+          <a-menu-item key="1">
+            Option 1
+          </a-menu-item>
+          <a-menu-item key="2">
+            Option 2
+          </a-menu-item>
+          <a-menu-item key="3">
+            Option 3
+          </a-menu-item>
+          <a-menu-item key="4">
+            Option 4
+          </a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <span slot="title"><a-icon type="appstore" /><span>Navigation Two</span></span>
+          <a-menu-item key="5">
+            Option 5
+          </a-menu-item>
+          <a-menu-item key="6">
+            Option 6
+          </a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub4">
+          <span slot="title"><a-icon type="setting" /><span>Navigation Three</span></span>
+          <a-menu-item key="9">
+            Option 9
+          </a-menu-item>
+          <a-menu-item key="10">
+            Option 10
+          </a-menu-item>
+          <a-menu-item key="11">
+            Option 11
+          </a-menu-item>
+          <a-menu-item key="12">
+            Option 12
+          </a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header :style="{ background: '#fff', padding: 0 }" />
+      <a-layout-content :style="{ margin: '24px 16px 0' }">
+        <div :style="{ padding: '24px', background: '#fff', minHeight: '600px' }">
+          content
+        </div>
+      </a-layout-content>
+      <a-layout-footer style="textAlign: center">
+        Ant Design ©2018 Created by Ant UED
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      rootSubmenuKeys: ['sub1', 'sub2', 'sub4'],
+      openKeys: ['sub1'],
+    };
+  },
+  methods: {
+    onOpenChange(openKeys) {
+      const latestOpenKey = openKeys.find(key => this.openKeys.indexOf(key) === -1);
+      if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+        this.openKeys = openKeys;
+      } else {
+        this.openKeys = latestOpenKey ? [latestOpenKey] : [];
+      }
+    },
+    onCollapse(collapsed, type) {
+      console.log(collapsed, type);
+    },
+    onBreakpoint(broken) {
+      console.log(broken);
+    }
+  }
+};
+</script>
 
-<style lang="scss">
-
+<style>
+#components-layout-demo-responsive {
+  height: 100%;
+  background: skyblue;
+}
+#components-layout-demo-responsive .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+}
+.ant-layout-sider-children{
+  background: #EFF1F4;
+}
+.ant-menu-root.ant-menu-inline{
+  background: #EFF1F4;
+}
+.ant-menu-sub.ant-menu-inline > .ant-menu-item{
+  background: #EFF1F4;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import { Button, message, Layout, Menu, Icon } from 'ant-design-vue';
+import App from './App';
+import 'ant-design-vue/dist/antd.css';
+
 import router from './router'
 import store from './store'
-
 Vue.config.productionTip = false
+
+Vue.component(Button.name, Button);
+Vue.component(Layout.name, Layout);
+Vue.component(Menu.name, Menu);
+Vue.component(Icon.name, Icon);
+Vue.component(Button.Group.name, Button.Group);
+Vue.use(Button);
+Vue.use(Layout);
+Vue.use(Menu);
+Vue.prototype.$message = message;
 
 new Vue({
   router,
